75d07cde0c99c8b60c5eba90686f6cae15dbbf10
Siniy <siniyili88888@gmail.com>
2022-08-04 17:13:35 +0300

    Сделали задание с суммой

---
+++ SHARP1/Lesson3/Program.cs
@@ -0,0 +1,9 @@
+﻿//int numberA = 3;
+//int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+
+int numberA = 34;
+int numberB = 51;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
