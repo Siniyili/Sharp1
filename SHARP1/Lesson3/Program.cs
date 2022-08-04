bbf067d183438c1aa0f6d1ea32ad3e3e01d3b3c0
Siniy <siniyili88888@gmail.com>
2022-08-04 17:35:33 +0300

    Добавил сложение случайных чисел

--- SHARP1/Lesson3/Program.cs
+++ SHARP1/Lesson3/Program.cs
@@ -2,8 +2,15 @@
 //int numberB = 5;
 //Console.WriteLine(numberA + numberB);
 
-int numberA = 34;
-int numberB = 51;
+//int numberA = 34;
+//int numberB = 51;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+int numberA = new Random().Next(1, 10);//1 2 3 4 5....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
