56d10abc291439a865d9c5ccc87bbf3fc4c2246c
Siniy <siniyili88888@gmail.com>
2022-08-04 18:04:22 +0300

    Неполучилось

---
+++ SHARP1/Lesson5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.write("Введите имя пользователя : ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "регина")
+{
+    Console.ReadLine("Ура, это же Регина!");
+}
+
+//else
+//{
+    //Console.write("Привет, ");
+    //Console.writeLine(username);
+//}
